e7a28cb6ed5f84690e90d7a9836d4428a10a5252
Mark Williams <github@shymedia.com>
2017-02-02 11:22:36 -0800

    Add our basic logo and basic server template pieces from another project.

---
+++ src/apiserver/apiserver.js
@@ -0,0 +1,69 @@
+/* Base imports */
+import { Server } from 'http';
+import bodyParser from 'body-parser';
+import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
+import passport from 'passport';
+/* Routes */
+import { accountRoutes, recipientRoutes, authenticationRoutes, postRoutes } from './modules';
+/* Configurations */
+import '../config/environment';
+//import mongooseConnection from '../config/mongoConnect';
+import redisClient from '../config/redisConnect';
+
+const RedisStore = require('connect-redis')(session);
+
+let port = process.env.API_SERVER_PORT;
+if (!port) {
+  port = 3009;
+}
+
+const app = express();
+
+/* Middleware setup */
+app.use((err, req, res, next) => {
+  if (res.headersSent) next(err);
+  res.status(err.status || port).render('500');
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  name: 'snss',
+  secret: 'MmyWTLNNsTi15L8n3iUH8kls',
+  resave: true,
+  saveUninitialized: false,
+  store: new RedisStore({ client: redisClient }),
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(morgan('combined'));
+
+/* Routes */
+app.use('/api/v1', [accountRoutes, recipientRoutes, authenticationRoutes, postRoutes]);
+
+app.get('/', function baseReturn(req, res) {
+  res.send('Hello - this is the api server. You probably want a more interesting endpoint.');
+});
+
+process.on('SIGTERM', () => {
+  console.log('Closing server.');
+  app.close();
+});
+
+app.on('close', () => {
+  console.log('Closing redis.');
+  redisClient.quit();
+  mongooseConnection.close();
+});
+
+/* Start the API Server */
+const server = Server(app);
+server.listen(port, function reportOnListen(error) {
+  if (error) {
+    console.log(`API Server ERROR on startup: ${error}`);
+  } else {
+    console.log(`API Server listening on http://localhost:${port}.`);
+  }
+});
+
