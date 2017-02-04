f2ee233138305730bafbd0b17f04baa7009e47b3
Mark Williams <github@shymedia.com>
2017-02-04 15:54:01 -0800

    Add relationship between posts and account

--- src/apiserver/models/Account.js
+++ src/apiserver/models/Account.js
@@ -2,82 +2,100 @@ import { encryptPassword, passwordsMatch } from './passwordEncryption';
 import { appraisePassword, appraiseEmail, appraiseDisplayName } from '../../shared/helpers/appraise';
 
 module.exports = (sequelize, DataTypes) => {
-  const Account = sequelize.define('Account', {
-    displayName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isValidDisplayName: function validateDisplayName(value) {
-          const appraisalMessages = appraiseDisplayName(value);
-          if (appraisalMessages.length > 0) {
-            throw new Error(appraisalMessages.join(' '));
-          }
-        },
-      },
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isValidEmail: function validateEmail(value) {
-          const appraisalMessages = appraiseEmail(value);
-          if (appraisalMessages.length > 0) {
-            throw new Error(appraisalMessages.join(' '));
-          }
-        },
-      },
-      unique: true,
-    },
-    encryptedPasswordHash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    encryptedPasswordPepperId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  }, {
-    instanceMethods: {
-      setPassword: function setPassword(password) {
-        const self = this;
-        return Promise.resolve(appraisePassword(password))
-          .then(function checkAppraisal(appraisalMessages) {
+  const Account = sequelize.define(
+    'Account', {
+      displayName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isValidDisplayName: function validateDisplayName(value) {
+            const appraisalMessages = appraiseDisplayName(value);
             if (appraisalMessages.length > 0) {
-              throw new Error(appraisalMessages.join(', '));
+              throw new Error(appraisalMessages.join(' '));
             }
-            return password;
-          })
-          .then(function runHashing(passwordValue) {
-            return encryptPassword(passwordValue);
-          })
-          .then((encryptedValue) => {
-            self.encryptedPasswordHash = encryptedValue.encrypted;
-            self.encryptedPasswordPepperId = encryptedValue.pepperId;
-          })
-          .catch((err) => {
-            throw err;
-          });
+          },
+        },
       },
-      toJSON: function stripValues() {
-        const values = Object.assign({}, this.get());
-        delete values.encryptedPasswordHash;
-        delete values.encryptedPasswordPepperId;
-        return values;
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isValidEmail: function validateEmail(value) {
+            const appraisalMessages = appraiseEmail(value);
+            if (appraisalMessages.length > 0) {
+              throw new Error(appraisalMessages.join(' '));
+            }
+          },
+        },
+        unique: true,
       },
-      comparePassword(candidate) {
-        return passwordsMatch(
-          candidate,
-          this.encryptedPasswordHash,
-          this.encryptedPasswordPepperId,
-        );
+      encryptedPasswordHash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      encryptedPasswordPepperId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
       },
     },
-  });
+    {
+      instanceMethods: {
+        setPassword: function setPassword(password) {
+          const self = this;
+          return Promise.resolve(appraisePassword(password))
+            .then(function checkAppraisal(appraisalMessages) {
+              if (appraisalMessages.length > 0) {
+                throw new Error(appraisalMessages.join(', '));
+              }
+              return password;
+            })
+            .then(function runHashing(passwordValue) {
+              return encryptPassword(passwordValue);
+            })
+            .then((encryptedValue) => {
+              self.encryptedPasswordHash = encryptedValue.encrypted;
+              self.encryptedPasswordPepperId = encryptedValue.pepperId;
+            })
+            .catch((err) => {
+              throw err;
+            });
+        },
+        toJSON: function stripValues() {
+          const values = Object.assign({}, this.get());
+          delete values.encryptedPasswordHash;
+          delete values.encryptedPasswordPepperId;
+          return values;
+        },
+        // TODO: We really need a profile table but this works for now.
+        toProfile: function stripForProfile() {
+          const values = Object.assign({}, this.get());
+          delete values.encryptedPasswordHash;
+          delete values.encryptedPasswordPepperId;
+          delete values.email;
+          return values;
+        },
+        comparePassword(candidate) {
+          return passwordsMatch(
+            candidate,
+            this.encryptedPasswordHash,
+            this.encryptedPasswordPepperId,
+          );
+        },
+      },
+    },
+    {
+      classMethods: {
+        associate: function associatModesl(models) {
+          Account.hasMany(models.Post, { foreignKey: 'accountId' });
+        },
+      },
+    },
+  );
   return Account;
 };
--- src/apiserver/models/Post.js
+++ src/apiserver/models/Post.js
@@ -1,21 +1,30 @@
 /* A post is the atomic messages of the user */
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    message: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Post = sequelize.define(
+    'Post', {
+      message: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      edited: {
+        type: DataTypes.BOOLEAN,
+      },
+      accountID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    edited: {
-      type: DataTypes.BOOLEAN,
-    },
-    accountID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  },
     {
       timestamps: true,
       paranoid: true,
-    });
+    },
+    {
+      classMethods: {
+        associate: function associateModels(models) {
+          Post.belongsTo(models.Account, { foreignKey: 'accountId' });
+        },
+      },
+    },
+  );
   return Post;
 };
