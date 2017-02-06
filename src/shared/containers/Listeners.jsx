4226bed3465348aefbe40764cb15a094c3223ad2
Mark Williams <github@shymedia.com>
2017-02-06 12:12:00 -0800

    Fix listeners to actually show the names of listeners

--- src/shared/containers/Listeners.jsx
+++ src/shared/containers/Listeners.jsx
@@ -17,20 +17,26 @@ const NoListeners = () => (
 );
 
 /* Renders a list of listeners */
-const AllListeners = ({ listeners }) => (
-  <div>
-    {listeners.map(listener => (
-      <Listener displayName={listener.displayName} />
-    ))}
-  </div>
-);
+const AllListeners = ({ listeners }) => {
+  const listenerComponents = [];
+  let listenerNumber = 1;
+  listeners.map((listener) => { // eslint-disable-line array-callback-return
+    listenerComponents.push(<Listener key={`listener${listenerNumber}`} displayName={listener} />);
+    listenerNumber += 1;
+  });
+  return (
+    <div>
+      {listenerComponents}
+    </div>
+  );
+};
 AllListeners.propTypes = {
   listeners: React.PropTypes.array, // eslint-disable-line react/forbid-prop-types
 };
 
 /* Main container that manages showing of posts */
 function Listeners({ listeners }) {
-  if (listeners.length === 0) {
+  if (!listeners || listeners.length === 0) {
     return (<NoListeners />);
   }
   return (
@@ -41,11 +47,14 @@ function Listeners({ listeners }) {
 Listeners.propTypes = {
   listeners: React.PropTypes.array, // eslint-disable-line react/forbid-prop-types
 };
+Listeners.defaultProps = {
+  listeners: [],
+};
 
 /** redux store map **/
 const mapStateToProps = function mapStateToProps(state) {
   return {
-    listeners: state.listeners,
+    listeners: state.listeners.listeners,
   };
 };
 
