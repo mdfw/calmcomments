0d4c2cb3f5e6383911625d5c817c7e99fa4f419b
Mark Williams <github@shymedia.com>
2017-02-05 21:55:00 -0800

    Add registerForm

---
+++ src/shared/components/RegisterForm.jsx
@@ -0,0 +1,181 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import CircularProgress from 'material-ui/CircularProgress';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router';
+
+/* SubmitProgress shows a spinner while we wait for account creation.
+  */
+class SubmitProgress extends React.Component { // eslint-disable-line react/no-multi-comp
+  render() {
+    if (this.props.submitting) {
+      return <CircularProgress mode="indeterminate" size={20} />;
+    }
+    return null;
+  }
+}
+SubmitProgress.propTypes = {
+  submitting: React.PropTypes.bool,
+};
+SubmitProgress.defaultProps = {
+  submitting: false,
+};
+
+const errorStyle = {
+  color: '#c94f49',
+  marginTop: '10px',
+  marginBottom: '5px',
+};
+
+const RegistrationError = ({ errorMessage }) => {
+  if (!errorMessage || errorMessage.length === 0) {
+    return null;
+  }
+  return <div style={errorStyle}>{errorMessage}</div>;
+};
+
+RegistrationError.propTypes = {
+  errorMessage: React.PropTypes.string,
+};
+RegistrationError.defaultProps = {
+  errorMessage: '',
+};
+
+
+/* button style for the submit button below */
+const submitButtonStyle = {
+  margin: 12,
+};
+
+/* Main registration form component */
+class RegisterForm extends React.Component { // eslint-disable-line react/no-multi-comp
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+  }
+  onChange(e) {
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+    this.props.handleChange({
+      [name]: value,
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit();
+  }
+  onBlur(e) {
+    const name = e.target.name;
+    this.props.handleBlur(name);
+  }
+  onFocus(e) {
+    const name = e.target.name;
+    this.props.handleFocus(name);
+  }
+  render() {
+    const {
+      submitting,
+      errors,
+      displayNameValue,
+      emailValue,
+      passswordValue,
+      passwordFieldType,
+    } = this.props;
+
+    let errorInfo = null;
+    if (errors.submitError) {
+      errorInfo = <RegistrationError errorMessage={errors.submitError} />;
+    }
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <div className="text-header">
+            Sign up for Social, Not Social
+          </div>
+          <div>
+            <Link to="/login" className="loginHints">or Log in</Link>
+          </div>
+          <div>
+            <TextField
+              name="displayName"
+              hintText="Your full name"
+              floatingLabelText="Your Name"
+              errorText={errors.displayName}
+              value={displayNameValue}
+              type="text"
+              disabled={submitting}
+              onChange={this.onChange}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+          </div>
+          <div>
+            <TextField
+              name="email"
+              hintText="Email"
+              floatingLabelText="Email"
+              value={emailValue}
+              errorText={errors.email}
+              type="text"
+              disabled={submitting}
+              onChange={this.onChange}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+          </div>
+          <div>
+            <TextField
+              name="password"
+              hintText=""
+              value={passswordValue}
+              floatingLabelText="Password"
+              type={passwordFieldType}
+              errorText={errors.password}
+              onChange={this.onChange}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+          </div>
+          {errorInfo}
+          <div>
+            <RaisedButton
+              label="Create account"
+              primary={true} // eslint-disable-line react/jsx-boolean-value
+              style={submitButtonStyle}
+              disabled={submitting || !errors.formReady}
+              type="submit"
+            />
+            <SubmitProgress submitting={submitting} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+RegisterForm.propTypes = {
+  handleSubmit: React.PropTypes.func.isRequired,
+  handleBlur: React.PropTypes.func.isRequired,
+  handleFocus: React.PropTypes.func.isRequired,
+  handleChange: React.PropTypes.func.isRequired,
+  submitting: React.PropTypes.bool,
+  errors: React.PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  displayNameValue: React.PropTypes.string,
+  emailValue: React.PropTypes.string,
+  passswordValue: React.PropTypes.string,
+  passwordFieldType: React.PropTypes.string,
+};
+RegisterForm.defaultProps = {
+  submitting: false,
+  errors: {},
+  displayNameValue: '',
+  emailValue: '',
+  passswordValue: '',
+  passwordFieldType: 'password',
+};
+
+export default RegisterForm;
---
+++ src/shared/containers/RegisterForm.jsx
@@ -0,0 +1,123 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { submitNewAccount, submitAccountErrorAck } from '../actions/account';
+import { formUpdate, REG_FORM_NAME } from '../actions/forms';
+import RegisterForm from '../components/RegisterForm';
+import { appraiseEmail,
+  appraiseDisplayName,
+  appraisePassword,
+} from '../helpers/appraise';
+
+function determineErrors(dname, email, pass, touched, exited) {
+  const errors = {
+    displayName: '',
+    email: '',
+    password: '',
+    formReady: true,
+  };
+  const displayNameErrors = appraiseDisplayName(dname);
+  if (displayNameErrors.length > 0 && exited.indexOf('displayName') > -1) {
+    errors.displayName = displayNameErrors.join(' ');
+  }
+  const emailErrors = appraiseEmail(email);
+  if (emailErrors.length > 0 && exited.indexOf('email') > -1) {
+    errors.email = emailErrors.join(' ');
+  }
+  const passwordErrors = appraisePassword(pass);
+  if (passwordErrors.length > 0 && ((exited.indexOf('password') > -1) || (touched.indexOf('password') > -1 && pass.length > 5))) {
+    errors.password = passwordErrors.join(' ');
+  }
+  if (passwordErrors.length > 0 || emailErrors.length > 0 || displayNameErrors.length > 0) {
+    errors.formReady = false;
+  }
+  return errors;
+}
+
+class RegisterFormContainer extends React.Component {
+  handleSubmit() {
+    this.props.dispatch(
+      submitNewAccount(this.props.displayName, this.props.email, this.props.password),
+    );
+  }
+  handleChange(fields) {
+    this.props.dispatch(formUpdate(REG_FORM_NAME, fields));
+  }
+  handleFocus(fieldName) {
+    if (this.props.fieldsTouched.indexOf(fieldName) === -1) {
+      const newTouched = this.props.fieldsTouched.concat(fieldName);
+      this.props.dispatch(formUpdate(REG_FORM_NAME, { fieldsTouched: newTouched }));
+    }
+  }
+  handleBlur(fieldName) {
+    if (this.props.fieldsExited.indexOf(fieldName) === -1) {
+      const newExited = this.props.fieldsExited.concat(fieldName);
+      this.props.dispatch(formUpdate(REG_FORM_NAME, { fieldsExited: newExited }));
+    }
+  }
+  handleErrorAck() {
+    this.props.dispatch(submitAccountErrorAck());
+  }
+  render() {
+    const errors = determineErrors(
+      this.props.displayName,
+      this.props.email,
+      this.props.password,
+      this.props.fieldsTouched,
+      this.props.fieldsExited,
+    );
+    if (this.props.submitError && this.props.submitError.length > 0) {
+      errors.submitError = this.props.submitError;
+    }
+    return (
+      <RegisterForm
+        handleSubmit={() => this.handleSubmit()}
+        handleChange={fields => this.handleChange(fields)}
+        handleBlur={fieldName => this.handleBlur(fieldName)}
+        handleFocus={fieldName => this.handleFocus(fieldName)}
+        handleErrorAck={() => this.handleErrorAck()}
+        submitting={this.props.submitting}
+        displayNameValue={this.props.displayName}
+        emailValue={this.props.email}
+        passswordValue={this.props.password}
+        passwordFieldType="password"
+        errors={errors}
+      />
+    );
+  }
+}
+
+RegisterFormContainer.propTypes = {
+  submitting: React.PropTypes.bool,
+  submitError: React.PropTypes.string,
+  dispatch: React.PropTypes.func.isRequired,
+  displayName: React.PropTypes.string,
+  email: React.PropTypes.string,
+  password: React.PropTypes.string,
+  fieldsTouched: React.PropTypes.arrayOf(React.PropTypes.string),
+  fieldsExited: React.PropTypes.arrayOf(React.PropTypes.string),
+};
+RegisterFormContainer.defaultProps = {
+  submitting: false,
+  submitError: null,
+  displayName: '',
+  email: '',
+  password: '',
+  fieldsTouched: [],
+  fieldsExited: [],
+};
+/** redux store map **/
+const mapStateToProps = function mapStateToProps(state) {
+  return {
+    submitting: state.account.submitting,
+    submitError: state.account.submitError,
+    displayName: state.forms.registerForm.displayName,
+    email: state.forms.registerForm.email,
+    password: state.forms.registerForm.password,
+    fieldsTouched: state.forms.registerForm.fieldsTouched,
+    fieldsExited: state.forms.registerForm.fieldsExited,
+  };
+};
+
+const Container = connect(mapStateToProps)(RegisterFormContainer);
+
+export default Container;
