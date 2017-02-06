20cbecdf3ace87930c22d9db3e522b0f9f0dc93c
Mark Williams <github@shymedia.com>
2017-02-06 10:54:03 -0800

    Add listenersReducer to the exported reducers.

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,6 +1,6 @@
 import accountReducer from './account';
 import formReducer from './forms';
 import postsReducer from './posts';
+import listenersReducer from './listeners';
 
-
-export { accountReducer, formReducer, postsReducer };
+export { accountReducer, formReducer, postsReducer, listenersReducer };
